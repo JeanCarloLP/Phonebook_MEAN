388bc71ea76df54c6bbb9f7424d4e09d5082b687
The Chan <jeancarloleonpr@gmail.com>
2019-10-30 17:34:41 +0100

    Frontend
    Template configuration
    Tests
    Phone Pattern issues

--- phonebook/src/app/app.module.ts
+++ phonebook/src/app/app.module.ts
@@ -8,17 +8,28 @@ import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 
+// Forms
+import { ReactiveFormsModule } from '@angular/forms';
+
 // HTTP
 import { HttpClientModule } from '@angular/common/http';
 
 // Material
 import { MatToolbarModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatOptionModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatTableModule } from '@angular/material';
+import { MatDividerModule } from '@angular/material';
+import { MatSnackBarModule } from '@angular/material';
 
 // Routes
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
-
-
 
 @NgModule({
   declarations: [
@@ -32,9 +43,22 @@ import { ROUTES } from './app.routes';
     BrowserAnimationsModule,
     RouterModule.forRoot( ROUTES, { useHash: true } ),
     MatToolbarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
